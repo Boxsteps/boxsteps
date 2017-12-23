613f5823046c7c1772b54fb8b88d4ec0005ac63f
Wolfgang Dielingen <wolfrainx@gmail.com>
2017-12-23 17:14:44 -0400

    Cambio en OAuth Google
    Cambio en la URL de callback de la verificación OAuth para manejar el dominio www.boxsteps.edu, ya que la extensión .dev no permite ser usada a través de HTTP.

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'google' => [
         'client_id' => '237856751091-8j3pu4p21evjcfn5h4rgm6uvc5g7e51k.apps.googleusercontent.com',
         'client_secret' => '18VhL8IxRcf-slXIvwwEjdcn',
-        'redirect' => 'http://www.boxsteps.dev/login/google/callback',
+        'redirect' => 'http://www.boxsteps.edu/login/google/callback',
     ],
 
 ];
