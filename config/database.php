f19cd6d0ec9fb162157bf3a54f1630459eca05b9
Wolfgang Dielingen <wolfrainx@gmail.com>
2018-01-16 00:09:50 -0400

    Actualización de configuración de base de datos
    Actualización del archivo de configuración de base de datos para permitir conectarse a través de un base de datos remota para ejecutar la aplicación en Heroku.

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'boxsteps_heroku'),
+            'username' => env('DB_USERNAME', 'boxsteps_heroku'),
+            'password' => env('DB_PASSWORD', 'Wolf5600*'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
